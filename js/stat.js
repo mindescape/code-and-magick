0bf73df189061c643dda3e996695ec76c92ffe4b
tomhet <escape.outerspace@gmail.com>
2018-10-30 04:23:08 +0500

    stat field painted on canvas

---
+++ js/stat.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var cloud = {
+  x: 110,
+  y: 10,
+  width: 420,
+  height: 270,
+  color: '#fff'
+};
+
+var cloudShadow = {
+  x: cloud.x + 10,
+  y: cloud.y + 10,
+  width: cloud.width,
+  height: cloud.height,
+  color: 'rgba(0, 0, 0, 0.3)'
+};
+
+var drawCloud = function (ctx, object, stroke) {
+  var offset = 10;
+  ctx.fillStyle = object.color;
+  ctx.beginPath();
+  ctx.moveTo(object.x, object.y);
+  ctx.lineTo(object.x + offset, object.y + (object.height / 2));
+  ctx.lineTo(object.x, object.y + object.height);
+  ctx.lineTo(object.x + object.width, object.y + object.height);
+  ctx.lineTo(object.x + object.width - offset, object.y + (object.height / 2));
+  ctx.lineTo(object.x + object.width, object.y);
+  ctx.lineTo(object.x, object.y);
+  ctx.fill();
+  ctx.closePath();
+  if (stroke) {
+    ctx.stroke();
+  }
+};
+
+var renderStatistics = function (ctx) {
+  drawCloud(ctx, cloudShadow, false);
+  drawCloud(ctx, cloud, true);
+};
